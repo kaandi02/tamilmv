2c2ddc8812b392d0cd6f6e6599107e7822945dcf
Kaandieepan Sathiyaseelan <52882824+kaandi02@users.noreply.github.com>
2021-10-03 10:25:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,17 +17,6 @@ function App() {
           <Route path="/" exact>
             <Video title="Videos" fetchURL={requests.fetchList} />
           </Route>
-          <Route path="/home" exact>
-            <h1 style={{color:"white"}}>Home Page</h1>
-          </Route>
-          <Route path="/upload" exact>
-            <div style={{color:"white",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontFamily:"Open Sans,sans-serif"}}>
-              <form encType="multipart/form-data" action="https://vv278o.dood.video/upload/01?54537foxgdvl9j21pplnf" method="post">
-                <input name="file" type="file" />
-                <input type="submit" />
-              </form>
-            </div>
-          </Route>
           <Route path="*">
             <h3 style={{color:"white",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontFamily:"Open Sans,sans-serif"}}>The Requested URL not found on this Server</h3>
           </Route>
